f09a8d416bf64aaeff1f83803f89f699ccc19275
Syed Amaanullah <syedamaanullah@Syeds-MacBook-Pro.local>
2015-01-13 10:19:51 -0600

    Initial commit

--- DragonFashion/ViewController.m
+++ DragonFashion/ViewController.m
@@ -9,6 +9,7 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
 
 @end
 
@@ -16,12 +17,9 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
+
 
 @end
